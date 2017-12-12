b3a79b58286aa6ce9e4fb2080d0961b373ff6125
Kristoffer J. Sivertsen <kristofferjoachim@gmail.com>
2017-12-12 16:16:53 +0100

    [test-studio] Better preview of geopoint

--- packages/test-studio/schemas/geopoint.js
+++ packages/test-studio/schemas/geopoint.js
@@ -1,3 +1,7 @@
+/*eslint-disable react/display-name, max-len */
+import React from 'react'
+import config from 'config:@sanity/google-maps-input?'
+
 export default {
   name: 'geopointTest',
   type: 'document',
@@ -14,5 +18,32 @@ export default {
       title: 'A geopoint',
       description: 'This is a geopoint field',
     }
-  ]
+  ],
+  preview: {
+    select: {
+      title: 'title',
+      geopoint: 'location'
+    },
+    prepare({title, geopoint}) {
+      return {
+        title: title,
+        media: options => {
+          if (!geopoint || !geopoint.lat || !geopoint.lng || !options || !config) {
+            return false
+          }
+          const height = (options.dimensions && options.dimensions.height) || 100
+          const width = (options.dimensions && options.dimensions.width) || 100
+          const img = (
+            <img
+              width={width}
+              height={height}
+              src={`https://maps.googleapis.com/maps/api/staticmap?key=${config.apiKey}&center=${geopoint.lat},${geopoint.lng}&size=${width}x${height}&maptype=roadmap&format=png&visual_refresh=true&scale=2&zoom=11`}
+              alt={title}
+            />
+          )
+          return img
+        }
+      }
+    }
+  }
 }
