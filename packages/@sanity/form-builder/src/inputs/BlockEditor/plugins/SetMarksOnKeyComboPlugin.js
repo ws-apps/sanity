bb0a4d2be9cddc377b8269f5774f49d6100eba30
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2017-12-16 10:10:23 +0100

    [form-builder] Block edtitor: Use hot-key module for setting up keyboard shortcuts

--- packages/@sanity/form-builder/src/inputs/BlockEditor/plugins/SetMarksOnKeyComboPlugin.js
+++ packages/@sanity/form-builder/src/inputs/BlockEditor/plugins/SetMarksOnKeyComboPlugin.js
@@ -1,35 +1,44 @@
 // @flow
+
+import isKeyHotkey, {toKeyName} from 'is-hotkey'
 import {Change} from 'slate'
+
 
 type Options = {
   decorators?: string[]
 }
 
-// This plugin inserts an empty default block after enter is pressed
-// within a block which is not a default block type.
-// I.e: when enter is pressed after a title, start a new empty normal block below
+// This plugin makes keyboard shortcuts for decorators
+
+const isStrongHotkey = isKeyHotkey('mod+b')
+const isEmphasisHotkey = isKeyHotkey('mod+i')
+const isUnderlinedHotkey = isKeyHotkey('mod+u')
+const isCodeHotKey = isKeyHotkey("mod+'")
+
+const modKeyPlatformName = toKeyName('mod')
+
+export const keyMaps = {
+  strong: `${modKeyPlatformName} + b`,
+  em: `${modKeyPlatformName} + i`,
+  underline: `${modKeyPlatformName} + u`,
+  code: `${modKeyPlatformName} + '`
+}
 
 export default function SetMarksOnKeyComboPlugin(options: Options = {}) {
   const decorators = options.decorators || []
   return {
     onKeyDown(event: SyntheticKeyboardEvent<*>, change: Change) {
-      const {key, metaKey} = event
-      if (!metaKey) {
-        return null
-      }
       let mark
-      switch (key) {
-        case 'b':
-          mark = 'strong'
-          break
-        case 'i':
-          mark = 'em'
-          break
-        case 'u':
-          mark = 'underline'
-          break
-        default:
-          return undefined
+      if (isStrongHotkey(event)) {
+        mark = 'strong'
+      } else if (isEmphasisHotkey(event)) {
+        mark = 'em'
+      } else if (isUnderlinedHotkey(event)) {
+        mark = 'underline'
+      } else if (isCodeHotKey(event)) {
+        mark = 'code'
+      } else {
+        return undefined
       }
       // Return if not supported by schema
       if (!decorators.includes(mark)) {
