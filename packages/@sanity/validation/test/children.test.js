92b89a5f6ce799b861306db6f2b3f84a2859f86b
Espen Hovlandsdal <espen@hovlandsdal.com>
2017-12-26 12:37:25 +0100

    [validation] Implement all() rule

--- packages/@sanity/validation/test/children.test.js
+++ packages/@sanity/validation/test/children.test.js
@@ -1,13 +1,52 @@
 const {Rule} = require('../src')
 
 describe('child rules', () => {
-  test('all() rules', () => {
+  test('all() rules - single failure', () => {
     const rule = Rule.string().all([
-      Rule.string().regex(/^[A-Z]/).error('Must start with an uppercase character'),
-      Rule.string().regex(/[a-z]+/).error('Must follow with lowercase characters')
+      Rule.string()
+        .regex(/^[A-Z]/)
+        .error('Must start with an uppercase character'),
+      Rule.string()
+        .regex(/[a-z]+/)
+        .error('Must follow with lowercase characters')
     ])
 
-    expect(rule.validate('moop')).toMatchSnapshot('all() rules - failure')
     expect(rule.validate('Sanity')).toMatchSnapshot('all() rules - match')
+    expect(rule.validate('moop')).toMatchSnapshot('all() rules - single failure, custom message')
+  })
+
+  test('all() rules - multiple failures', () => {
+    const rule = Rule.string().all([
+      Rule.string()
+        .regex(/^[A-Z]/)
+        .error('Must start with an uppercase character'),
+      Rule.string()
+        .min(5)
+        .error('5 chars or more'),
+      Rule.string()
+        .regex(/[a-z]+/)
+        .error('Must follow with lowercase characters')
+    ])
+
+    expect(rule.validate('Sanity')).toMatchSnapshot('all() rules - match')
+    expect(rule.validate('moop')).toMatchSnapshot(
+      'all() rules - multiple failures, custom messages'
+    )
+  })
+
+  test('all() rules - single failure, custom, common error', () => {
+    const rule = Rule.string()
+      .all([Rule.string().regex(/^[A-Z]/), Rule.string().regex(/[a-z]+/)])
+      .error('Needs to start with a capital letter and then follow with lowercase characters')
+
+    expect(rule.validate('moop')).toMatchSnapshot('all() rules - single failure, common error')
+  })
+
+  test('all() rules - single failure, custom, common error', () => {
+    const rule = Rule.string()
+      .all([Rule.string().regex(/^[A-Z]/), Rule.string().min(5), Rule.string().regex(/[a-z]+/)])
+      .error('Needs to be a capital letter followed by at least 4 lowercase characters')
+
+    expect(rule.validate('moop')).toMatchSnapshot('all() rules - multiple failures, common error')
   })
 })
