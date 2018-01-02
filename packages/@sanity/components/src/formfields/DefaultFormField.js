8d575f82ae83274d3fb29820560c145d29f71972
Espen Hovlandsdal <espen@hovlandsdal.com>
2018-01-02 11:56:59 +0100

    [components] Remove validation from props being passed down to inputs

--- packages/@sanity/components/src/formfields/DefaultFormField.js
+++ packages/@sanity/components/src/formfields/DefaultFormField.js
@@ -13,46 +13,78 @@ export default class DefaultFormField extends React.Component {
     level: PropTypes.number,
     children: PropTypes.node,
     wrapped: PropTypes.bool,
-    labelFor: PropTypes.string
+    labelFor: PropTypes.string,
+    validation: PropTypes.shape({
+      warnings: PropTypes.arrayOf(PropTypes.instanceOf(Error)),
+      errors: PropTypes.arrayOf(PropTypes.instanceOf(Error))
+    })
   }
 
   static defaultProps = {
     level: 1
   }
 
+  getValidationClass() {
+    const validation = this.props.validation
+    if (validation && validation.errors.length > 0) {
+      return styles.validationError
+    }
+
+    if (validation && validation.warnings.length > 0) {
+      return styles.validationWarning
+    }
+
+    return ''
+  }
+
+  renderValidationResult(validation) {
+    const hasErrors = validation && validation.errors.length > 0
+    const hasWarnings = validation && validation.warnings.length > 0
+    if (!hasErrors && !hasWarnings) {
+      return null
+    }
+
+    const messages = hasErrors ? validation.errors : validation.warnings
+    return <ul>{messages.map((err, i) => <li key={i}>{err.message}</li>)}</ul>
+  }
+
   render() {
+    const {
+      level,
+      label,
+      labelFor,
+      description,
+      children,
+      inline,
+      wrapped,
+      className,
+      validation
+    } = this.props
 
-    const {level, label, labelFor, description, children, inline, wrapped, className} = this.props
-
+    const validationClass = this.getValidationClass()
     const levelClass = `level_${level}`
 
     return (
       <div
         className={`
           ${inline ? styles.inline : styles.block}
+          ${validationClass}
           ${styles[levelClass] || ''}
           ${wrapped ? styles.wrapped : ''}
-          ${className || ''}`
-        }
+          ${className || ''}`}
       >
         <label className={styles.inner} htmlFor={labelFor}>
-          {label && <DefaultLabel
-            className={styles.label}
-            level={level}
-          >
-            {label}
-          </DefaultLabel>}
+          {label && (
+            <DefaultLabel className={styles.label} level={level}>
+              {label}
+            </DefaultLabel>
+          )}
 
-          {
-            description
-            && <div className={styles.description}>
-              {description}
-            </div>
-          }
-          <div className={styles.content}>
-            {children}
-          </div>
+          {description && <div className={styles.description}>{description}</div>}
 
+          <div className={styles.content}>{children}</div>
+
+          {this.renderValidationResult(validation)}
         </label>
       </div>
     )
