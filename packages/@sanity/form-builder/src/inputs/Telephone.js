a374dece1fd0dacd07a93c650c92b1702192fe93
Kristoffer J. Sivertsen <kristofferjoachim@gmail.com>
2018-01-23 15:23:22 +0100

    [form-builder] Pass validation down to text input

--- packages/@sanity/form-builder/src/inputs/Telephone.js
+++ packages/@sanity/form-builder/src/inputs/Telephone.js
@@ -33,11 +33,14 @@ export default class TelephoneInput extends React.Component<Props> {
 
   render() {
     const {value, markers, type, level, ...rest} = this.props
+    const validation = markers.filter(marker => marker.type === 'validation') || []
+    const errors = validation.filter(marker => marker.level === 'error') || []
 
     return (
       <FormField markers={markers} level={level} label={type.title} description={type.description}>
         <TextInput
           {...rest}
+          customValidity={errors && errors.length > 0 ? errors[0].item.message : ''}
           type="tel"
           value={value}
           readOnly={type.readOnly}
