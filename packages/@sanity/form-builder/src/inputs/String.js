7d46869a84bd8b6acf45ba8f5a637569e8cf63ee
Espen Hovlandsdal <espen@hovlandsdal.com>
2018-01-24 13:57:53 +0100

    [validation] Debounce validation by 300ms

--- packages/@sanity/form-builder/src/inputs/String.js
+++ packages/@sanity/form-builder/src/inputs/String.js
@@ -33,14 +33,14 @@ export default class StringInput extends React.Component<Props> {
 
   render() {
     const {value, type, markers, level, ...rest} = this.props
-    const validation = markers.filter(marker => marker.type === 'validation') || []
-    const errors = validation.filter(marker => marker.level === 'error') || []
+    const validation = markers.filter(marker => marker.type === 'validation')
+    const errors = validation.filter(marker => marker.level === 'error')
 
     return (
       <FormField markers={markers} level={level} label={type.title} description={type.description}>
         <TextInput
           {...rest}
-          customValidity={errors && errors.length > 0 ? errors[0].item.message : ''}
+          customValidity={errors.length > 0 ? errors[0].item.message : ''}
           type="text"
           value={value}
           readOnly={type.readOnly}
