3f7b26aebd9cd98bccef08338e35cc37815ca405
Espen Hovlandsdal <espen@hovlandsdal.com>
2018-01-24 14:27:05 +0100

    [validation] Fix proptypes on ValidationList

--- packages/@sanity/components/src/validation/ValidationList.js
+++ packages/@sanity/components/src/validation/ValidationList.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styles from './styles/ValidationList.css'
-import WarningIcon from 'part:@sanity/base/warning-icon'
 
 export default class ValidationList extends React.PureComponent {
   static propTypes = {
     markers: PropTypes.arrayOf(
       PropTypes.shape({
-        path: PropTypes.arrayOf(PropTypes.string),
+        path: PropTypes.arrayOf(
+          PropTypes.oneOfType([
+            PropTypes.string,
+            PropTypes.number,
+            PropTypes.shape({_key: PropTypes.string})
+          ])
+        ),
         type: PropTypes.string,
         level: PropTypes.string,
         item: PropTypes.any
@@ -26,38 +31,28 @@ export default class ValidationList extends React.PureComponent {
     const warnings = validation.filter(marker => marker.level === 'warning') || []
     return (
       <div className={styles.root}>
-        {
-          errors && errors.length > 0 && (
+        {errors &&
+          errors.length > 0 && (
             <div className={styles.errors}>
               <h3>Errors</h3>
               <ul>
-                {
-                  errors.map((error, i) => {
-                    return (
-                      <li key={i}>{error.item.message}</li>
-                    )
-                  })
-                }
+                {errors.map((error, i) => {
+                  return <li key={i}>{error.item.message}</li>
+                })}
               </ul>
             </div>
-          )
-        }
-        {
-          warnings && warnings.length > 0 && (
+          )}
+        {warnings &&
+          warnings.length > 0 && (
             <div className={styles.warnings}>
               <h3>Warnings</h3>
               <ul>
-                {
-                  warnings.map((warning, i) => {
-                    return (
-                      <li key={i}>{warning.item.message}</li>
-                    )
-                  })
-                }
+                {warnings.map((warning, i) => {
+                  return <li key={i}>{warning.item.message}</li>
+                })}
               </ul>
             </div>
-          )
-        }
+          )}
       </div>
     )
   }
